9520c0d4709902c11275edc554f430b33dcda9a9
Anir-sadiqui <anirsadiqui2@gmail.com>
2025-03-24 13:31:21 +0100

    commit

--- Api_Diab.py
+++ Api_Diab.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
         app, 
-        host="127.0.0.1", 
+        host="0.0.0.0", 
         port=8000,
         log_level="info"
     )
